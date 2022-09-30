efe4e8c7a93ec2aa8791a1e972ee46e4fe100f53
Ikars <lyxikars123@gmail.com>
2022-09-30 17:46:27 +0300

    Fixed messaging and added simple searching

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,21 +12,47 @@ public class IndexModel : PageModel
     public List<Listing> Listings { get; set; } = new List<Listing>();
     private readonly IEntityRepository<Listing> _listingRepository;
     public int PageNumber = 1;
-    public string NextPage => Url.ActionLink("Index", "Home", values: new { listingPage = PageNumber + 1 }) ?? "/Index";
-    public string PrevPage => Url.ActionLink("Index", "Home", values: new { listingPage = Math.Max(1, PageNumber - 1) }) ?? "/Index";
+    public string? SearchParam { get; set; } = string.Empty;
+    public string NextPage => Url.ActionLink("Index", "Home", values: new { listingPage = PageNumber + 1, search = SearchParam }) ?? "/Index";
+    public string PrevPage => Url.ActionLink("Index", "Home", values: new { listingPage = Math.Max(1, PageNumber - 1), search = SearchParam }) ?? "/Index";
     public IndexModel(ILogger<IndexModel> logger, IEntityRepository<Listing> listingRepository)
     {
         _logger = logger;
         _listingRepository = listingRepository;
     }
 
-    public async void OnGetAsync(int? listingPage)
+    public async Task OnPostAsync([FromForm] string? search, [FromRoute] int? listingPage)
+    {
+        Listings = await GetListings(listingPage, search);
+    }
+    public async Task OnGetAsync([FromRoute] string? search, [FromRoute] int? listingPage)
+    {
+        // Izdabūjam pēdējos 24 
+        Listings = await GetListings(listingPage, search);
+    }
+
+    private async Task<List<Listing>> GetListings(int? listingPage = 1, string? searchParameter=null)
     {
         PageNumber = listingPage ?? 1;
         PageNumber = Math.Max(1, Math.Min(PageNumber, (await _listingRepository.Count() / 24)));
-        
-        // Izdabūjam pēdējos 24 
-        Listings = await _listingRepository.AsQueryable()
-            .Include(l => l.Images).OrderByDescending(l => l.Created).Skip((PageNumber - 1) * 24).Take(24).ToListAsync();
+        var query = _listingRepository.AsQueryable()
+            .Include(l => l.Images);
+        if (!string.IsNullOrEmpty(searchParameter))
+        {
+            string formattedSearch = searchParameter ?? string.Empty;
+            formattedSearch = formattedSearch.ToLowerInvariant();
+            SearchParam = searchParameter;
+            return await query.Where(l =>
+                l.Title.ToLowerInvariant().Contains(formattedSearch) ||
+                l.Description.ToLowerInvariant().Contains(formattedSearch))
+                .OrderByDescending(l => l.Created)
+                .Skip((PageNumber - 1) * 24).Take(24)
+                .ToListAsync();
+        }
+
+        return await query
+            .OrderByDescending(l => l.Created)
+            .Skip((PageNumber - 1) * 24).Take(24)
+            .ToListAsync();
     }
 }
